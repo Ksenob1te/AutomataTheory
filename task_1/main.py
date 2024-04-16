9caa737c693e90d7ffd4a6640738b97704b16415
Ksenob1te <beta.ksenobite@mail.ru>
2024-04-16 11:32:31 +0300

    split files

--- task_1/main.py
+++ task_1/main.py
@@ -2,6 +2,7 @@ import random
 import string
 from typing import Tuple
 import time
+import sys
 
 from regex import resolver_regex
 from smc import resolver_smc
@@ -12,55 +13,37 @@ from gen import incorrect_generator
 
 # irc://server2:25565/randomname?password
 if __name__ == '__main__':
+    print(sys.argv)
     random.seed(time.time())
-    selected_servername = [
-        "SERVER_FIRST",
-        "SERVER_SECOND",
-        "SERVER_THIRD",
-        "SERVER_FOURTH",
-        "SERVER_FIFTH",
-        "SERVER_SIXTH",
-        "SERVER_SEVENTH"
-    ]
-    def generate_random() -> Tuple[str, int]:
-        x = random.randint(0, 2)
-        s = ""
-        if x == 0:
-            s = incorrect_generator.get_string()
-        elif x == 1:
-            s = correct_generator.get_string()
-        elif x == 2:
-            s = correct_generator.get_string(random.choice(selected_servername))
-        return s, x
 
     lex_timer, smc_timer, regex_timer = 0, 0, 0
     server_dict = {}
     incorrect_iter = 0
     selected_s_count = 0
 
-    input_file = open("input.txt", "w")
+    def check(resolver_type, checker_s: str) -> Tuple[bool, float]:
+        global server_dict, incorrect_iter
+        result, timer = resolver_type(checker_s)
+        # if (result is None and type_s != 0) or (result is not None and type_s == 0):
+        #     incorrect_iter += 1
+        if result:
+            server_dict[result] = server_dict.get(result, 0) + 1
+        return bool(result), timer
 
-    for i in range(1, 100000):
-        checker_s, type_s = generate_random()
-        # print(i, "(" + checker_s + ")")
-        input_file.write(checker_s + "\n")
+    for line in sys.stdin:
+        line = line.replace("\n", "")
+        if "lex" in sys.argv:
+            result, lex_timer = check(lexer.resolve, line)
+            print(f"Lex parser: {result} in {lex_timer}")
+        if "smc" in sys.argv:
+            result, smc_timer = check(resolver_smc.resolve, line)
+            print(f"Smc parser: {result} in {smc_timer}")
+        if "regex" in sys.argv:
+            result, regex_timer = check(resolver_regex.resolve, line)
+            print(f"Regex parser: {result} in {smc_timer}")
 
-
-        def check(resolver_type):
-            global server_dict, incorrect_iter, checker_s
-            result, timer = resolver_type(checker_s)
-            if (result is None and type_s != 0) or (result is not None and type_s == 0):
-                incorrect_iter += 1
-            if result:
-                server_dict[result] = server_dict.get(result, 0) + 1
-            return timer
-
-        lex_timer += check(lexer.resolve)
-        smc_timer += check(resolver_smc.resolve)
-        regex_timer += check(resolver_regex.resolve)
-
-        if type_s == 2:
-            selected_s_count += 1
-    input_file.close()
+        # if type_s == 2:
+        #     selected_s_count += 1
+        # input_file.close()
     print(smc_timer, regex_timer, lex_timer, incorrect_iter)
     # print(server_dict[selected_servername] / 3, selected_s_count)
---
+++ task_1/print_lines.py
@@ -0,0 +1,30 @@
+import random
+from typing import Tuple
+from gen import incorrect_generator, correct_generator
+
+selected_servername = [
+    "SERVER_FIRST",
+    "SERVER_SECOND",
+    "SERVER_THIRD",
+    "SERVER_FOURTH",
+    "SERVER_FIFTH",
+    "SERVER_SIXTH",
+    "SERVER_SEVENTH"
+]
+
+def generate_random() -> Tuple[str, int]:
+    global selected_servername
+    x = random.randint(0, 2)
+    s = ""
+    if x == 0:
+        s = incorrect_generator.get_string()
+    elif x == 1:
+        s = correct_generator.get_string()
+    elif x == 2:
+        s = correct_generator.get_string(random.choice(selected_servername))
+    return s, x
+
+
+if __name__ == "__main__":
+    for i in range(10000):
+        print(generate_random()[0])
